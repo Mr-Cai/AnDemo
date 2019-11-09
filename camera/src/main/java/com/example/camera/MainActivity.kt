9f2fa151bd4e22b95b4e0b1b5eed08bda2def7b2
蔡凯歌 <caikaige.g@gmail.com>
2019-11-09 18:49:55 +0800

    更新库版本号

--- camera/src/main/java/com/example/camera/MainActivity.kt
+++ camera/src/main/java/com/example/camera/MainActivity.kt
@@ -1,30 +1,19 @@
 package com.example.camera
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.graphics.Matrix
+import android.app.Dialog
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.util.Log
-import android.util.Rational
-import android.util.Size
-import android.view.Surface
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
-import java.nio.ByteBuffer
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        captureBTN.setOnClickListener {
+            Dialog(this).apply {
+                setContentView(R.layout.hello)
+                show()
+            }
+        }
     }
-
 }
