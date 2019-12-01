17e851eb4f656b5787539d2b1c24d4bc50158bfa
蔡凯歌 <caikaige.g@gmail.com>
2019-12-02 00:53:45 +0800

    视图模型绑定数据源、本地缓存数据 View & ViewModel

--- forecast/src/main/java/com/example/forecast/ui/weather/current/TodayFragment.kt
+++ forecast/src/main/java/com/example/forecast/ui/weather/current/TodayFragment.kt
@@ -2,46 +2,41 @@ package com.example.forecast.ui.weather.current
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.forecast.R
-import com.example.forecast.data.WeatherService
-import com.example.forecast.data.network.NetworkDataSourceImpl
-import com.example.forecast.data.network.NetworkInterceptorImpl
+import com.example.forecast.ui.base.ScopeFragment
 import kotlinx.android.synthetic.main.week_fragment.*
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class TodayFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = TodayFragment()
-    }
-
+class TodayFragment : ScopeFragment(), KodeinAware {
+    override val kodein by closestKodein()
+    private val todayFactory by instance<TodayFactory>()
     private lateinit var viewModel: TodayViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.today_fragment, container, false)
-    }
+    ) = inflater.inflate(R.layout.today_fragment, container, false)!!
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(TodayViewModel::class.java)
-        val weatherService = WeatherService(NetworkInterceptorImpl(this.context!!))
-        val weatherDataSource = NetworkDataSourceImpl(weatherService)
-        weatherDataSource.downloaderNowWeather.observe(this, Observer {
+        viewModel = ViewModelProviders.of(this, todayFactory).get(TodayViewModel::class.java)
+        bindUI()
+    }
+
+    private fun bindUI() = launch {
+        val nowWeather = viewModel.weather.await()
+        nowWeather.observe(this@TodayFragment, Observer {
+            if (it == null) return@Observer
             textView.text = it.toString()
         })
-        GlobalScope.launch(Dispatchers.Main) {
-            weatherDataSource.getTodayWeatherAsync("深圳")
-        }
     }
 
 }
